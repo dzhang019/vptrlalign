8e49c2fc33ff86cd9102165141f456e8827183ec
Melvin Huang <63562169+mhuang24@users.noreply.github.com>
2025-02-20 12:52:44 -0500

    Reverted to original starting environment

--- training.py
+++ training.py
@@ -14,7 +14,7 @@ from lib.tree_util import tree_map
 from lib.reward_structure_mod import custom_reward_function
 from lib.policy_mod import compute_kl_loss
 from torchvision import transforms
-from night_hard import HumanSurvivalNight
+from minerl.herobraine.env_specs.human_survival_specs import HumanSurvival
 
 th.autograd.set_detect_anomaly(True)
 
