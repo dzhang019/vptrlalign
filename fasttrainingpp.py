ad263dd1777e02ecfe59f588eec7756559ab8442
Daniel Zhang <dtz2104@columbia.edu>
2025-03-07 00:57:13 -0500

    making sure parallel processing properly passes sequential data to transformer5

--- fasttrainingpp.py
+++ fasttrainingpp.py
@@ -476,6 +476,7 @@ def train_rl_mp(
         p.daemon = True
         p.start()
         workers.append(p)
+        time.sleep(0.5)
     
     # Thread stop flag (for clean shutdown)
     thread_stop = [False]
