efb39617f4931cbbc7b3829c21c03392e1b07040
Melvin Huang <63562169+mhuang24@users.noreply.github.com>
2025-03-14 14:46:12 -0400

    Update training.sh

--- training.sh
+++ training.sh
@@ -14,16 +14,21 @@ kl=50.0 #this too
 iterations=100
 
 #Phase 1
+echo "Training Phase 1"
 python3 vptrlalign/fasttrainingpp2.py --in-model foundation-model-2x.model --in-weights rl-from-house-2x.weights --out-weights "$folder/phase1.weights" --out-episodes log.txt --num-iterations $iterations --rollout-steps 80 --num-envs 11 --queue-size 3 --temp $temp --lambda-kl $kl --reward lib.phase1
 
 #Phase 2
+echo "Training Phase 2"
 python3 vptrlalign/fasttrainingpp2.py --in-model foundation-model-2x.model --in-weights "$folder/phase1.weights" --out-weights "$folder/phase2.weights" --out-episodes log.txt --num-iterations $iterations --rollout-steps 80 --num-envs 11 --queue-size 3 --temp $temp --lambda-kl $kl --reward lib.phase2
 
 #Phase 3
+echo "Training Phase 3"
 python3 vptrlalign/fasttrainingpp2.py --in-model foundation-model-2x.model --in-weights "$folder/phase2.weights" --out-weights "$folder/phase3.weights" --out-episodes log.txt --num-iterations $iterations --rollout-steps 80 --num-envs 11 --queue-size 3 --temp $temp --lambda-kl $kl --reward lib.phase3
 
 #Phase 4
+echo "Training Phase 4"
 python3 vptrlalign/fasttrainingpp2.py --in-model foundation-model-2x.model --in-weights "$folder/phase3.weights" --out-weights "$folder/phase4.weights" --out-episodes log.txt --num-iterations $iterations --rollout-steps 80 --num-envs 11 --queue-size 3 --temp $temp --lambda-kl $kl --reward lib.phase4
 
 #Phase 5
+echo "Training Phase 5"
 python3 vptrlalign/fasttrainingpp2.py --in-model foundation-model-2x.model --in-weights "$folder/phase4.weights" --out-weights "$folder/phase5.weights" --out-episodes log.txt --num-iterations $iterations --rollout-steps 80 --num-envs 11 --queue-size 3 --temp $temp --lambda-kl $kl --reward lib.phase5
