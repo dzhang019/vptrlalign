304de803be60ed165bed123cfc5038c761040911
Melvin Huang <63562169+mhuang24@users.noreply.github.com>
2025-03-14 13:42:33 -0400

    Update training.sh

--- training.sh
+++ training.sh
@@ -1,16 +1,18 @@
 mkdir curriculum
+temp=2 #Change this hyperparameter
+kl=50.0 #this too
 
 #Phase 1
-python3 vptrlalign/fasttrainingpp2.py --in-model 2x.model --in-weights rl-from-house-2x.weights --out-weights curriculum/phase1.weights --out-episodes log.txt --num-iterations 100 --rollout-steps 80 --num-envs 11 --queue-size 3 --temp 2 --lambda-kl 50 --reward lib.phase1
+python3 vptrlalign/fasttrainingpp2.py --in-model 2x.model --in-weights rl-from-house-2x.weights --out-weights curriculum/phase1.weights --out-episodes log.txt --num-iterations 100 --rollout-steps 80 --num-envs 11 --queue-size 3 --temp $temp --lambda-kl $kl --reward lib.phase1
 
 #Phase 2
-python3 vptrlalign/fasttrainingpp2.py --in-model 2x.model --in-weights curriculum/phase1.weights --out-weights curriculum/phase2.weights --out-episodes log.txt --num-iterations 100 --rollout-steps 80 --num-envs 11 --queue-size 3 --temp 2 --lambda-kl 50 --reward lib.phase2
+python3 vptrlalign/fasttrainingpp2.py --in-model 2x.model --in-weights curriculum/phase1.weights --out-weights curriculum/phase2.weights --out-episodes log.txt --num-iterations 100 --rollout-steps 80 --num-envs 11 --queue-size 3 --temp $temp --lambda-kl $kl --reward lib.phase2
 
 #Phase 3
-python3 vptrlalign/fasttrainingpp2.py --in-model 2x.model --in-weights curriculum/phase2.weights --out-weights curriculum/phase3.weights --out-episodes log.txt --num-iterations 100 --rollout-steps 80 --num-envs 11 --queue-size 3 --temp 2 --lambda-kl 50 --reward lib.phase3
+python3 vptrlalign/fasttrainingpp2.py --in-model 2x.model --in-weights curriculum/phase2.weights --out-weights curriculum/phase3.weights --out-episodes log.txt --num-iterations 100 --rollout-steps 80 --num-envs 11 --queue-size 3 --temp $temp --lambda-kl $kl --reward lib.phase3
 
 #Phase 4
-python3 vptrlalign/fasttrainingpp2.py --in-model 2x.model --in-weights curriculum/phase3.weights --out-weights curriculum/phase4.weights --out-episodes log.txt --num-iterations 100 --rollout-steps 80 --num-envs 11 --queue-size 3 --temp 2 --lambda-kl 50 --reward lib.phase4
+python3 vptrlalign/fasttrainingpp2.py --in-model 2x.model --in-weights curriculum/phase3.weights --out-weights curriculum/phase4.weights --out-episodes log.txt --num-iterations 100 --rollout-steps 80 --num-envs 11 --queue-size 3 --temp $temp --lambda-kl $kl --reward lib.phase4
 
 #Phase 5
-python3 vptrlalign/fasttrainingpp2.py --in-model 2x.model --in-weights curriculum/phase4.weights --out-weights curriculum/phase2.weights --out-episodes log.txt --num-iterations 100 --rollout-steps 80 --num-envs 11 --queue-size 3 --temp 2 --lambda-kl 50 --reward lib.phase5
+python3 vptrlalign/fasttrainingpp2.py --in-model 2x.model --in-weights curriculum/phase4.weights --out-weights curriculum/phase5.weights --out-episodes log.txt --num-iterations 100 --rollout-steps 80 --num-envs 11 --queue-size 3 --temp $temp --lambda-kl $kl --reward lib.phase5
