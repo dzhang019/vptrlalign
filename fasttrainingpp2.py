bea1e45a8d7c4dc3741a1120bc9c8fa82d24271a
Melvin Huang <63562169+mhuang24@users.noreply.github.com>
2025-03-18 22:43:05 -0400

    Update fasttrainingpp2.py

--- fasttrainingpp2.py
+++ fasttrainingpp2.py
@@ -20,7 +20,7 @@ from lib.reward_structure_mod import custom_reward_function
 from lib.policy_mod import compute_kl_loss
 from torchvision import transforms
 from minerl.herobraine.env_specs.human_survival_specs import HumanSurvival
-from lib.phase1 import LogsAndIronSwordEnv, register_custom_env
+from custom_minerl_env import LogsAndIronSwordEnv, register_custom_env
 from torch.cuda.amp import autocast, GradScaler
 
 # Modified version to integrate with your existing code
@@ -1062,7 +1062,7 @@ def training_thread(agent, pretrained_policy, rollout_queue, stop_flag, num_iter
             total_steps += num_transitions
             avg_loss = running_loss / total_steps if total_steps > 0 else 0.0
             LAMBDA_KL *= KL_DECAY
-def train_rl_mp(
+def train_rl_mp_with_custom_env(
     in_model,
     in_weights,
     out_weights,
