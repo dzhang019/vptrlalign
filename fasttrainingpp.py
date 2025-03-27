ecf322d71ef748b481ed945590b70ced0dc1b4e1
Daniel Zhang <dtz2104@columbia.edu>
2025-03-26 21:12:12 -0400

    adjust hyperparams

--- fasttrainingpp.py
+++ fasttrainingpp.py
@@ -931,14 +931,14 @@ def training_thread(agent, pretrained_policy, rollout_queue, stop_flag, num_iter
         phase_coordinator: Coordinator for synchronizing phases between threads
     """
     # Hyperparameters
-    LEARNING_RATE = 1e-5
+    LEARNING_RATE = 1.2e-5
     MAX_GRAD_NORM = 1.0
     # LAMBDA_KL = 0.2
     #march 26 mod
     LAMBDA_KL =  0.13
     GAMMA = 0.995
     LAM = 0.95
-    VALUE_LOSS_COEF = 1
+    VALUE_LOSS_COEF = 1.3
     KL_DECAY = 0.9995
     
     # PPG specific hyperparameters
