67ea646391979b2322bc06c315f5991523a20208
Daniel Zhang <dtz2104@columbia.edu>
2025-03-28 11:44:29 -0400

    testing for gradnorm

--- fasttrainingpp.py
+++ fasttrainingpp.py
@@ -163,6 +163,7 @@ def env_worker(env_id, action_queue, result_queue, stop_flag, HumanSurvivalClass
             if done:
                 custom_reward -= 30.0
             custom_reward/=4
+            custom_reward = 0
             # Increment step count
             episode_step_count += 1
             
